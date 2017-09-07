c933a2820a23aa13d9d422cdeccb3e90ad0bb342
Igor Moiseyenko <imoiseyenko93@gmail.com>
2017-09-08 01:32:31 +0300

    Routing: usage of Home & AboutUs components.

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,5 +1,16 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Home from './Home'
+import AboutUs from './AboutUs'
 
-const App = () => <h1>Hello ChitChat Material Design!</h1>
+const App = () => (
+  <Router>
+    <div>
+      <h1>Hello ChitChat Material Design!</h1>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={AboutUs} />
+    </div>
+  </Router>
+)
 
 export default App
