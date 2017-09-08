476c57760a025691bd4b8c2509181093d9e14faf
Sergey Moiseyenko <smoiseyenko@gmail.com>
2017-09-08 22:39:11 +0300

    add Logo & NavigationList to Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,24 +1,13 @@
 import React from 'react'
-import AppBar from 'material-ui/AppBar'
-import Tabs, { Tab } from 'material-ui/Tabs'
+import NavigationList from '../NavigationList/index'
+import Logo from '../Logo/index'
+import './index.css'
 
 const Header = () => (
-  <AppBar position="static" color="default">
-    <Tabs
-      value={0}
-      onChange={() => {}}
-      indicatorColor="primary"
-      textColor="primary"
-      fullWidth
-    >
-      <Tab label="Item One" />
-      <Tab label="Item Two" />
-      <Tab label="Item Three" />
-      <Tab label="Item Four" />
-      <Tab label="Item Five" />
-      <Tab label="Item Six" />
-    </Tabs>
-  </AppBar>
+  <div className="header">
+    <Logo />
+    <NavigationList />
+  </div>
 )
 
 export default Header
