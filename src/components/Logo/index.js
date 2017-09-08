ee30f07afd66125fe9f316be7367b1318a1e4502
Sergey Moiseyenko <smoiseyenko@gmail.com>
2017-09-08 22:41:50 +0300

    add NavLink to Logo component

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import './index.css'
 
-const Logo = () => <div className="logo">LOGO</div>
+const Logo = () => (
+  <NavLink to="/">
+    <div className="logo">LOGO</div>
+  </NavLink>
+)
 
 export default Logo
