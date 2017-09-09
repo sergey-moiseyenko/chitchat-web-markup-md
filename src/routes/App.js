980a675894186285b622d0991408d671974f03b1
Igor Moiseyenko <imoiseyenko93@gmail.com>
2017-09-09 04:07:28 +0300

    Set default color scheme.

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles'
+import { deepPurple } from 'material-ui/colors'
 import Home from './Home'
 import AboutUs from './AboutUs'
 
-const theme = createMuiTheme()
+const theme = createMuiTheme({
+  mainColorPalette: deepPurple
+})
 
 const App = () => (
   <MuiThemeProvider theme={theme}>
