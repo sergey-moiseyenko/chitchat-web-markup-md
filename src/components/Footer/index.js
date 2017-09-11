c730c078b3c23b35cb582bab0010d30d89765a00
Sergey Moiseyenko <smoiseyenko@gmail.com>
2017-09-11 13:17:33 +0300

    Footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { withStyles } from 'material-ui'
+import styles from './styles'
+
+const Footer = ({ classes }) => (
+  <div className={classes.footer}>
+    <span>© 2017 Chit-Chat</span>
+  </div>
+)
+
+export default withStyles(styles)(Footer)
---
+++ src/components/Footer/styles.js
@@ -0,0 +1,9 @@
+export default ({ mainColorPalette, spacing }) => ({
+  footer: {
+    color: mainColorPalette[500],
+    backgroundColor: mainColorPalette[100],
+    paddingTop: spacing.unit * 2,
+    paddingBottom: spacing.unit * 2,
+    paddingLeft: spacing.unit * 2
+  }
+})
